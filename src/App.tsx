74720a217a96c865db8263f17ffe0349cfd2272f
lucile <developer.lvt@gmail.com>
2023-10-18 22:37:16 +0200

    fixed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 import LoginPage from './pages/login/LoginPage';
 import DashboardPage from './pages/dashboard/DashboardPage';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginPage />}>
-        </Route>
-        <Route path='/dashboard' element={<DashboardPage />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <LoginPage></LoginPage>
   );
 }
 
