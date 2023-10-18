192e34d84b773b6ebec7e60b7da7156fe57d8981
lucile <developer.lvt@gmail.com>
2023-10-18 17:41:52 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
 
   return (
-    <LoginPage></LoginPage>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />}>
+        </Route>
+        <Route path='/dashboard' element={<DashboardPage />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
