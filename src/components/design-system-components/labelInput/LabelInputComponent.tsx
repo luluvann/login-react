96197d77d2d1581d867a40ba76b2d776f625e9ee
lucile <developer.lvt@gmail.com>
2023-10-18 16:14:04 +0200

    created label input component

---
+++ src/components/design-system-components/labelInput/LabelInputComponent.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import './LabelInputComponent.scss'
+import { ValidationResult } from '../../../helpers/validators';
+
+export enum LabelVariant {
+    LIGHT = "light",
+    DARK = "dark",
+}
+
+export enum InputType {
+    EMAIL = "email",
+    PASSWORD = "password",
+    DATE = "date",
+}
+
+
+export interface LabelInput {
+    inputType: InputType,
+    onInputChange: (value: string) => void;
+    id?: number,
+    labelText?: string,
+    labelVariant?: LabelVariant,
+    fitContent?: boolean,
+    inputPlaceholder?: string,
+    inputValidator?: ValidationResult,
+    saveAttempt?: boolean,
+}
+
+type Props = {
+    properties: LabelInput,
+}
+
+function LabelInputComponent(props: Props) {
+    const [inputValue, setInputValue] = useState('');
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
+        props.properties.onInputChange(newValue); // Pass the input value to the parent component
+    };
+
+    return (
+        <div className={`label-input__wrapper ${props.properties.inputValidator?.valid === false && props.properties.saveAttempt ? 'error' : ''}`}>
+            {
+                props.properties.labelText && (
+                    <label className={`${props.properties.labelVariant}`}>{props.properties.labelText} {props.properties.saveAttempt}</label>
+                )
+            }
+            <input
+               
+                type={props.properties.inputType}
+                placeholder={props.properties.inputPlaceholder}
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+            <div className='font-error-color1'>{props.properties.inputValidator?.message}</div>
+
+        </div>
+    );
+}
+
+export default LabelInputComponent;
