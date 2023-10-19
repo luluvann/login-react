869d518ad37720271529d82228eee5456095a538
lucile <developer.lvt@gmail.com>
2023-10-19 13:57:37 +0200

    styled dashboard page

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
+import './DashboardPage.scss'
 
 function DashboardPage() {
 
     return (
-        <>
+        <div className='dashboard'>
             <div className='font-regular-color7'>You have successfully logged in! This page will be protected later!</div>
             <Link to={{
                 pathname: '/login-react',
             }}>Go back to Login page</Link>
-        </>
+        </div>
     );
 
 }
